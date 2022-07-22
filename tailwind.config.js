2bb67ec3a7ea79227e931a613bb587fe61f8f576
Pablo Valenzuela <3bc41pnvc2010@gmail.com>
2022-07-22 13:04:52 -0400

    cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class',
-    content: ["./public/index.html",
+    content: ["index.html",
     "./src/**/*.{html,js}"],
     theme: {
         colors: {
